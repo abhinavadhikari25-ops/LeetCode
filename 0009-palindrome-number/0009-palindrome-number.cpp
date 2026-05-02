b655a17b3e5dca88deaeab43fe44dc0850cdce95
abhinavadhikari25-ops <abhinavadhikari25@gmail.com>
2026-05-02 23:40:38 +0530

    Time: 1 ms (57.55%), Space: 8.6 MB (64.99%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        int digit;
+        long rev = 0;
+        int temp = x;
+        while(x!=0){
+            digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x/10; 
+        }
+        if (rev == temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
