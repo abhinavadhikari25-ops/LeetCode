fa211686dddddb06426deccb269c87d6350958b6
abhinavadhikari25-ops <abhinavadhikari25@gmail.com>
2026-08-30 13:48:32 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.3%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans ;
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            total += nums[i];
+            ans.push_back(total);
+        }
+        return ans;
+    }
+};
